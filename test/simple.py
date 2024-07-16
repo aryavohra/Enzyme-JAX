28f318ffc8c3150cfe3fdd9bd153b6f8844f8bab
smjleo <seojun.lee@oriel.ox.ac.uk>
2024-07-16 21:15:08 +0100

    add conditional and custom Applier rewrites

--- test/simple.py
+++ test/simple.py
@@ -4,8 +4,8 @@ import jax.random
 import jax.lax
 import enzyme_ad.jax as enzyme_jax
 
-def test(x, y):
-    return x[0:1, 2:3]
+def test(x, y, z, w):
+    return jnp.concat([x, jnp.concat([y, z]), w])
 
 class Simple(absltest.TestCase):
     def test_simple_random(self):
@@ -15,8 +15,10 @@ class Simple(absltest.TestCase):
         
         a = jnp.array([[1.0, 2.0, 3.0, 0.0], [4.0, 5.0, 6.0, 0.0]])
         b = jnp.array([[6.0, 5.0, 4.0, 0.0], [3.0, 2.0, 1.0, 0.0]])
+        c = jnp.array([[3.0, 5.0, 4.0, 2.0], [1.0, 4.0, 9.0, 0.0]])
+        d = jnp.array([[6.0, 5.0, 4.0, 3.0], [3.0, 6.0, 1.0, 5.0]])
         
-        eres = efunc(a, b)
+        eres = efunc(a, b, c, d)
         print("enzyme forward", eres)
 
 if __name__ == "__main__":
