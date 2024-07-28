9f82d846d3f52116365a183d9564968d712a43a8
Arya Vohra <arya.vohra04@gmail.com>
2024-07-28 01:13:23 -0400

    Update EqualitySaturation.h
    need to add more ops to cost model

--- src/enzyme_ad/jax/Passes/EqualitySaturation.h
+++ src/enzyme_ad/jax/Passes/EqualitySaturation.h
@@ -6,38 +6,38 @@
 #include "rust/cxx.h"
 
 namespace tensat {
-        enum class Type: uint8_t;
+enum class Type : uint8_t;
+enum class Ops : uint8_t;
 
-        /**
-         * Functions exposed to Rust (Tensat) for getting the cost of new operations.
-         */
-        class CostModel {
-                public:
-                        uint64_t getAddOpCost(rust::Slice<const int64_t> lhsDims,
-                                        Type lhsType,
-                                        rust::Slice<const int64_t> rhsDims,
-                                        tensat::Type rhsType) const;
-                        uint64_t getMulOpCost(rust::Slice<const int64_t> lhsDims,
-                                        Type lhsType,
-                                        rust::Slice<const int64_t> rhsDims,
-                                        tensat::Type rhsType) const;
-                        uint64_t getSubtractOpCost(rust::Slice<const int64_t> lhsDims,
-                                        Type lhsType,
-                                        rust::Slice<const int64_t> rhsDims,
-                                        tensat::Type rhsType) const;
-                        uint64_t getDivOpCost(rust::Slice<const int64_t> lhsDims,
-                                        Type lhsType,
-                                        rust::Slice<const int64_t> rhsDims,
-                                        tensat::Type rhsType) const;
+/**
+ * Functions exposed to Rust (Tensat) for getting the cost of new operations.
+ */
+class CostModel {
+public:
+  // TODO: Our operands should be tuples of (shape, type).
+  // SHOULD TensorData have a type field too?
+  uint64_t getBinOpCost(Ops op, rust::Slice<const int64_t> lhsDims,
+                        Type lhsType, rust::Slice<const int64_t> rhsDims,
+                        tensat::Type rhsType) const;
 
-                private:
-                        mlir::Type newTensorType(mlir::OpBuilder &builder,
-                                        rust::Slice<const int64_t> dims,
-                                        tensat::Type type) const;
-                        mlir::Type tensatTypeToMlirType(mlir::OpBuilder &builder,
-                                        tensat::Type type) const;
-        };
+  static mlir::Type newTensorType(mlir::OpBuilder &builder,
+                           rust::Slice<const int64_t> dims,
+                           tensat::Type type);
+  static mlir::Type tensatTypeToMlirType(mlir::OpBuilder &builder,
+                                  tensat::Type type);
+};
 
-        std::unique_ptr<tensat::CostModel> newCostModel();
-}
+class ShapeInference {
+public:
 
+  rust::Vec<int> get_shape(
+    Ops op,
+    rust::Slice<const rust::Slice<const int64_t>> operand_dims,
+    rust::Slice<const Type> operand_types,
+    rust::Slice<const rust::Slice<const int64_t>> other_vector_args,
+    rust::Slice<const int64_t> int_args) const;
+};
+
+std::unique_ptr<tensat::CostModel> newCostModel();
+std::unique_ptr<tensat::ShapeInference> newShapeInference();
+} // namespace tensat
